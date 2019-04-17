80a84a84dd90cf997c3d13282d6c2ed156602060
CobraKai1117 <43760180+CobraKai1117@users.noreply.github.com>
2019-04-17 15:19:14 -0500

    Water Lerp

--- WaterProject/Assets/Scenes/Scenarios/Module6/Mod6 Scripts/BottleScript.cs
+++ WaterProject/Assets/Scenes/Scenarios/Module6/Mod6 Scripts/BottleScript.cs
@@ -3,31 +3,208 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BottleScript : MonoBehaviour
-{
-    [SerializeField]GameObject PH7Flash;
-    [SerializeField]GameObject PH10Flash;
+{[SerializeField] List<GameObject> ListOfPoints = new List<GameObject>();
+
+
+    [SerializeField] GameObject PH7Flash;
+    [SerializeField] GameObject PH10Flash;
     [SerializeField] GameObject PH7Bottle;
     [SerializeField] GameObject PH10Bottle;
     [SerializeField] GameObject ProbePt;
-    float lerpTime;
-    bool lerping;
+    [SerializeField] GameObject MoveDownPt;
+    [SerializeField] GameObject MoveRightPt;
+    [SerializeField] float lerpTime;
+
+    [SerializeField] bool lerping;
+    [SerializeField] int count;
+
+    int currentStartPoint;
+    float journeyLength;
+    float startTime;
+    float speed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        lerping = true;
+        currentStartPoint = 0;
+
+         lerping = true;
         FlashDisAppear(PH7Flash);
         FlashDisAppear(PH10Flash);
+        count = 0;
 
         
     }
 
     // Update is called once per frame
     void Update()
-    { if (lerping)
-        { lerpTime += Time.deltaTime;
-            Vector3.Lerp(PH10Bottle.transform.position, ProbePt.transform.position,1);
+    {
+        Time.timeScale = 1;
+        if (lerping == true)
+        {
+            lerpTime += Time.deltaTime;
+            switch (count)
+            {
+                case 0:
+
+                    LerpObj(PH7Bottle, ListOfPoints[count], .4f * lerpTime);
+
+                    if (0.4f * lerpTime >= 1)
+                    {
+                        count++;
+                        lerpTime = 0;
+                    }
+                    break;
+
+                case 1:
+
+                    LerpObj(PH7Bottle, ListOfPoints[count], .4f * lerpTime);
+
+                    if (0.4f * lerpTime >= 1)
+                    {
+                        count++;
+                        lerpTime = 0;
+                    }
+                    break;
+
+                case 2:
+
+                    LerpObj(PH7Bottle, ListOfPoints[count], .4f * lerpTime);
+
+                    if (0.4f * lerpTime >= 1)
+                    {
+                        count++;
+                        lerpTime = 0;
+                    }
+                    break;
+
+                case 3:
+                    LerpObj(PH7Bottle, ListOfPoints[count], .4f * lerpTime);
+                    if (0.4 * lerpTime >=1)
+                    {
+                        count++;
+                        lerpTime = 0;
+                        
+                    }
+                    break;
+
+                case 4:
+                    LerpObj(PH7Bottle, ListOfPoints[count], .4f * lerpTime);
+                    if (0.4 * lerpTime >=1)
+                    {
+                        count++;
+                        lerpTime = 0;
+                    }
+                    break;
+
+
+                case 5:
+                    LerpObj(PH7Bottle, ListOfPoints[count], .4f * lerpTime);
+                    if (0.4 * lerpTime >=1)
+                    {
+                        count++;
+                        lerpTime = 0;
+                    }
+
+                    break;
+
+
+                case 6:
+                    LerpObj(PH10Bottle, ListOfPoints[count], .4f * lerpTime);
+                    if (0.4 * lerpTime >=1)
+                    {
+                        count++;
+                        lerpTime = 0;
+                    }
+                    break;
+
+                case 7:
+                    LerpObj(PH10Bottle, ListOfPoints[count], .4f * lerpTime);
+                    if (0.4 * lerpTime >=1)
+                    {
+                        count++;
+                        lerpTime = 0;
+                    }
+                    break;
+
+
+                case 8:
+                    LerpObj(PH10Bottle, ListOfPoints[count], .4f * lerpTime);
+                    {
+                        if (0.4 * lerpTime >=1)
+                        {
+                            count++;
+                            lerpTime = 0;
+                        }
+                    }
+
+                    break;
+
+
+                case 9:
+                    LerpObj(PH10Bottle, ListOfPoints[count], .4f * lerpTime);
+
+                    if (0.4 * lerpTime >=1)
+                    {
+                        count++;
+                        lerpTime = 0;
+                    }
+
+                    break;
+
+                case 10:
+                    LerpObj(PH10Bottle, ListOfPoints[count], .4f * lerpTime);
+                    if (0.4 * lerpTime > 1)
+                    {
+                        count++;
+                        lerpTime = 0;
+                    }
+                    break;
+
+                case 11:
+                    LerpObj(PH10Bottle, ListOfPoints[count], .4f * lerpTime);
+                    if (0.4 * lerpTime > 1)
+                    {
+                        count++;
+                        lerpTime = 0;
+                    }
+                    break;
+               
+            }
+
         }
-        
+
+
+   /*  Vector3 LerpPosOne =Vector3.Lerp(PH7Bottle.transform.position, MoveDownPt.transform.position, .6f * lerpTime);
+
+       PH7Bottle.transform.position = LerpPosOne;
+
+
+       Vector3 LerpPosTwo = Vector3.Lerp(LerpPosOne, MoveRightPt.transform.position, .6f * lerpTime);
+       PH7Bottle.transform.position = LerpPosTwo;
+
+       Vector3 LerpPosThree = Vector3.Lerp(LerpPosTwo, ProbePt.transform.position, .6f * lerpTime); */
+
+
+
+        /* float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        PH10Bottle.transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+        if (fracJourney >=1f && currentStartPoint + 1 < waypoints.Length)
+        {
+            currentStartPoint++;
+            SetPoints();
+        } */
+
+
+
+        /*if (lerping == true && count ==0)
+        { lerpTime += Time.deltaTime;
+            LerpObj(PH7Bottle, MoveDownPt, .6f * lerpTime);
+            LerpObj(PH7Bottle, MoveRightPt, .6f * lerpTime);
+            LerpObj(PH7Bottle, ProbePt, .6f * lerpTime);
+         
+        } */
+
     }
 
     public void FlashAppear(GameObject ObjToFlash)
@@ -39,5 +216,15 @@ public class BottleScript : MonoBehaviour
     public void FlashDisAppear(GameObject ObjToDisappear)
     {
         ObjToDisappear.SetActive(false);
+    }
+
+    public void LerpObj(GameObject LerpObj, GameObject LerpPt, float speed)
+    {
+        LerpObj.transform.position = Vector3.Lerp(LerpObj.transform.position, LerpPt.transform.position, speed);
+    }
+
+    void SetPoints()
+    {
+    
     }
 }
